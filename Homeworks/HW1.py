ddbcd80747bd5c97170b7b3f928bb291f7adcc60
Necip <necipsaylan@gmail.com>
2021-03-11 20:53:22 +0300

    20210311

---
+++ Homeworks/HW1.py
@@ -0,0 +1,11 @@
+odd = [1, 3, 5, 7, 9]
+even = [0, 2, 4, 6, 8]
+oddm = []
+evenm = []
+for i in odd:
+    oddm.append(i*2)
+for i in even:
+    evenm.append(i*2)
+for a in zip (oddm,evenm):
+    print(a)
+    print(type(a))
---
+++ Homeworks/HW2.py
@@ -0,0 +1,10 @@
+first = {"Name": "Ahmet", "Surname": "Balaban", "Age": 40, "City": "Istanbul", "Gender": "Male", "Date": 1980}
+second = {"Name": "Ali", "Surname": "Bal", "Age": 30, "City": "Izmir", "Gender": "Male", "Date": 1990}
+third = {"Name": "Ceyda", "Surname": "Alan", "Age": 25, "City": "Istanbul", "Gender": "Female", "Date": 1995}
+fourth = {"Name": "Davut", "Surname": "Sag", "Age": 38, "City": "Ankara", "Gender": "Male", "Date": 1982}
+fifth = {"Name": "Ekrem", "Surname": "Caglar", "Age": 43, "City": "Antalya", "Gender": "Male", "Date": 1978}
+print(first)
+print(second)
+print(third)
+print(fourth)
+print(fifth)
