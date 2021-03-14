405d6120b91a3cd3c292b48e52ca5875312b56de
Necip <necipsaylan@gmail.com>
2021-03-14 20:45:20 +0300

    20210314

--- Final Project/Final_Project.py
+++ Final Project/Final_Project.py
@@ -1,3 +1,80 @@
-import pandas as pd
+r=0
+w=0
+print ("Welcome to Knowledge Competition!!!")
+print ("Answer the following questions!")
+print ("Q1: What is the command that returns the number of items from a sequence or characters in a string?")
+A1 = input("Enter your answer: ").lower
+if A1 == "len":
+    r+=1
+else:
+    w+=1
+print ("Q2: Which command converts a string to an integer?")
+A2 = input("Enter your answer: ").lower
+if A2 == "int":
+    r+=1
+else:
+    w+=1
+print ("Q3: Which method splits a string into separate phrases?")
+A3 = input("Enter your answer: ").lower
+if A3 == "split":
+    r+=1
+else:
+    w+=1
+print ("Q4: Which method sorts a list from smallest to highest or a string alphabetically?")
+A4 = input("Enter your answer: ").lower
+if A4 == "sorted":
+    r+=1
+else:
+    w+=1
+print ("Q5: What is '!='?")
+A5 = input("Enter your answer: ").lower
+if A5 == "not equal":
+    r+=1
+else:
+    w+=1
+print ("Q6: Which method adds input to the end of a list?")
+A6 = input("Enter your answer: ").lower
+if A6 == "append":
+    r+=1
+else:
+    w+=1
+print ("Q7: Which command converts a string to uppercase?")
+A7 = input("Enter your answer: ").lower
+if A7 == "upper":
+    r+=1
+else:
+    w+=1
+print ("Q8: Which command interrupt the (loop) cycle?")
+A8 = input("Enter your answer: ").lower
+if A8 == "break":
+    r+=1
+else:
+    w+=1
+print ("Q9: Which command exits the function and returns a value?")
+A9 = input("Enter your answer: ").lower
+if A9 == "return":
+    r+=1
+else:
+    w+=1
+print ("Q10: What creates a new anonymous function?")
+A10 = input("Enter your answer: ").lower
+if A10 == "lambda":
+    r+=1
+else:
+    w+=1
 
-data = pd.read_csv("project.csv")
+print("Your competition has finished.\n\n")
+
+if r > 5:
+    print(f"{r} questions are correct. You are successful")
+else:
+    print(f"{w} questions are false. You are unsuccessful")
+
+
+
+
+
+
+
+
+
--- Homeworks/HW3.py
+++ Homeworks/HW3.py
@@ -1,5 +1,18 @@
-#Explain your work
+student0 =[]
+student1=[]
+student2=[]
+student3=[]
+student4=[]
+for i in range (6):
+	midterm=float(input("Enter your midterm grade: "))
+	student(i).append(midterm)
+	project=float(input("Enter your project grade: "))
+	student(i).append(project)
+	final= float(input("Enter your final grade"))
+	student(i).append(final
+	Passing=(midterm*(0.3) + project*(0.3) + final*(0.4))
+	GradeDict("Midterm Grade:" midterm(i), "Project Grade:" project(i), "Final Grade:" final(i), "Passing Grade:" passing(i))
+	print("Grades of the student(i):", GradeDict)
 
-#Question 1
-for x in range(a):
-	print(a)
+
+
